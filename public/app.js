ba4e1abdd32ec3f2d56e302c390dc5c1bc0c6e4d
AlohaJarren <calizo.jarren@gmail.com>
2018-12-07 13:03:21 -1000

    derp

--- public/app.js
+++ public/app.js
@@ -1,30 +1,45 @@
-axios
-    .get('https://szfd5uljeg.execute-api.us-west-2.amazonaws.com/dev/get')
-    .then((response)=>{
+// axios
+//     .get('https://szfd5uljeg.execute-api.us-west-2.amazonaws.com/dev/get')
+//     .then((response)=>{
         // var student1 = response.data.message.rows;
         // console.log('student1[0]', student1[0]);
         // document.getElementById('derp').innerHTML = "Name : " + student1[0].name;
         // document.getElementById('derp2').innerHTML = "Id : " + student1[0].id;
         // document.getElementById('derp3').innerHTML = "Grade : " + student1[0].grade;
-        console.log('response', response.data.message.rows);
-        const putIn = (response.data.message.rows).map(x=>{
-            console.log('x', x);
-            let cardTemplate = `
-            <div class = 'derp4'>
-                <p> Id : ${x.id} </p>
-                <p> Name : ${x.name} </p> 
-                <p> Grade : ${x.grade} </p>
-            </div>
-            `
-            let ofu = document.createElement('div');
-            ofu.innerHTML = cardTemplate;
-            document.querySelector('#derp').appendChild(ofu);
-        })
-    })
-    .catch((error)=>{
-        console.log('error', error);
-    })
+    //     console.log('response', response.data.message.rows);
+    //     const putIn = (response.data.message.rows).map(x=>{
+    //         console.log('x', x);
+    //         let cardTemplate = `
+    //         <div class = 'derp4'>
+    //             <p> Id : ${x.id} </p>
+    //             <p> Name : ${x.name} </p> 
+    //             <p> Grade : ${x.grade} </p>
+    //         </div>
+    //         `
+    //         let ofu = document.createElement('div');
+    //         ofu.innerHTML = cardTemplate;
+    //         document.querySelector('#derp').appendChild(ofu);
+    //     })
+    // })
+    // .catch((error)=>{
+    //     console.log('error', error);
+    // })
 
+const name = document.querySelector('#post-name');
+const grade = document.querySelector('#post-geade');
+const submit = document.querySelector('#post-submit');
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('name.value', name.value);
+    console.log('grade.value', grade.value);
+    axios
+        .post('https://szfd5uljeg.execute-api.us-west-2.amazonaws.com/dev/post', {
+            name: name.value;
+        })
+})
+
+//1-3 9-26
     // const htmlTemplateBuilder = response =>{
     //     console.log('response', response);
     //     result.innerHTML = response.data.message.rows
---
+++ routes/post.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Pool = require('pg-pool');
+const config = require('../config.json');
+const { table, host, database, user, password, port } = config;
+const pool = new Pool({
+  host,
+  database,
+  user,
+  password,
+  port,
+  idleTimeoutMillis: 1000
+});
+
+module.exports.postStudent = (event, context, callback) => {
+console.log('event', event);
+// console.log('name', name);
+// console.log('grade', grade);
+  const postStudent = `INSERT INTO ${table} VALUES(default, $1, $2);`;
+  let {name, grade} = event.body;
+  
+  pool.connect()
+    .then(client => {
+      client.release();
+      return client.query(postStudent, [name, grade]);
+    })
+    .then(data => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: JSON.stringify({
+          message: data,
+          input: event,
+        }),
+      };
+      callback(null, response)
+    })
+
+    .catch(error => {
+      console.log('error', error);
+    })
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
